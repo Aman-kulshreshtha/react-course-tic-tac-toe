1b2bff4e767dca897a1d43c9e917fc45b29fe8f7
amankulshreshtha <aman21102000@gmail.com>
2024-05-12 21:25:39 +0530

    created reusable player components

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,11 +1,12 @@
-export default function Player() {
+export default function Player({name, symbol}) {
 
+    // return player details
     return (<li>
         <span className="player">
           <span className="player-name">
-            Player 1
+            {name}
           </span>
-          <span className="player-symbol">X</span>
+          <span className="player-symbol">{symbol}</span>
           </span>
         <button>Edit</button>
         </li>);
