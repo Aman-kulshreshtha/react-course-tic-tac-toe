8cf8dd13b22533b971256b20d58678175952f76b
amankulshreshtha <aman21102000@gmail.com>
2024-05-12 21:25:08 +0530

    created reusable player components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import Player from "./components/Player/Player"
 
 function App() {
   
 
   return (
-    <p>Coming soon ....</p>
+    <main>
+
+      {/* the main container that holds the gAME */}
+      <div id="game-container">
+
+        {/* An ordered list that tells the player details */}
+        <ol id="players">
+
+          {/* player detail */} 
+          <Player name="Player 1" symbol={"X"} />
+
+          {/* player detail */} 
+          <Player name="Player 2" symbol={"O"} />
+        </ol>
+
+
+        Board
+      </div>
+    </main>
   )
 }
 
